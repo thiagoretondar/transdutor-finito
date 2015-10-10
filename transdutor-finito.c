0977a24364ee218cb190fa383b670d80b4033754
Thiago Retondar <t.retondar@gmail.com>
2015-10-10 20:28:39 -0300

    Transdutor reconhecendo variáveis, porém com sendo repetidas

--- transdutor-finito.c
+++ transdutor-finito.c
@@ -5,38 +5,49 @@
 #define true 1
 #define false 0
 
-
-char tempS[200];
-char tempN[200];
-
 /* Cabeçalhos */
+int ehLetra(char);
+int ehNumero(char);
+int ehBranco(char);
+int ehFinalPalavra(char);
+
 void e0();
 void e1();
 void e2();
 
 // delta1
-void insereEmTempS();
+void insereEmTempS(char);
 
 // delta2
-void anexaEmTempS();
+void anexaEmTempS(char);
 
 // delta3
 void finalizaTempS();
 
 // delta4
-void insereEmTempN();
+void insereEmTempN(char);
 
 // delta5
-void anexaEmTempN();
+void anexaEmTempN(char);
 
 // delta6
 void finalizaTempN();
 
-int main(int argc, char const *argv[]) { 
+/* Variáveis globais */
+// TODO: fazer palavra ser um input
+char palavra[200] = "AB A A28 A C";
+char tabelaVariaveis[20][10];
+char tempS[200];
+char tempN[200];
 
-	// TODO: fazer palavra ser um input
-	char palavra = "AB A 28 A28 A C";
-	int posicaoAtual = 0;
+int posicaoPalavra = 0;
+int posicaoMatrizVariaveis = 0;
+int posicaoTempS = 1;
+int posicaoTempN = 1;
+
+
+
+int main(int argc, char const *argv[]) { 
 
 	e0();
 
@@ -45,26 +56,98 @@ int main(int argc, char const *argv[]) {
 
 void e0() {
 
-	if (ehLetra(palavra)) {
-		insereEmTempS(palavra, posicaoAtual);
+	char simbolo = palavra[posicaoPalavra];
+
+	if (ehLetra(simbolo)) {
+		insereEmTempS(simbolo);
+		posicaoPalavra++;
 		e1();
-	} else if (ehNumero(palavra)) {
+	} /*else if (ehNumero(simbolo)) {
 		e2();
-	}
+	}*/
 }
 
 void e1() {
 
-	if (ehLetra(palavra) || ehNumero(palavra)) {
-		anexaEmTempS();
+	char simbolo = palavra[posicaoPalavra];
+
+	if (ehLetra(simbolo) || ehNumero(simbolo)) {
+		anexaEmTempS(simbolo);
+		posicaoPalavra++;
 		e1();
+	} else if (ehBranco(simbolo) || ehFinalPalavra(simbolo)) {
+		finalizaTempS();
+		posicaoPalavra++;
+		e0();
 	}
 }
 
 void e2() {
 
-	if (ehNumero(palavra)) {
-		anexaEmTempN();
+	if (ehNumero(palavra[posicaoPalavra])) {
+		//anexaEmTempN(palavra[posicaoPalavra]);
 		e2();
 	}
+}
+
+int ehLetra(char simbolo) {
+
+	if (simbolo >= 'A' && simbolo <= 'Z' || 
+		simbolo >= 'a' && simbolo <= 'z') {
+		return true;
+	}
+
+	return false;
+}
+
+int ehNumero(char simbolo) {
+
+	if (simbolo >= '0' && simbolo <= '9') {
+		return true;
+	}
+
+	return false;
+}
+
+int ehBranco(char simbolo) {
+
+	if (simbolo == ' ') {
+		return true;
+	}
+
+	return false;
+}
+
+int ehFinalPalavra(char simbolo) {
+
+	if (simbolo == '\0') {
+		return true;
+	}
+
+	return false;
+}
+
+void insereEmTempS(char simbolo) {
+
+	tempS[0] = simbolo;
+}
+
+void anexaEmTempS(char simbolo) {
+
+	tempS[posicaoTempS] = simbolo;
+	posicaoTempS++;
+}
+
+void finalizaTempS() {
+
+	// Encerra a string de tempS
+	tempS[posicaoTempS] = '\0';
+
+	strcpy(tabelaVariaveis[posicaoMatrizVariaveis], tempS);
+
+	posicaoMatrizVariaveis++;
+
+	posicaoTempS = 1;
+
+	printf("variavel = %s\n", tempS);
 }
